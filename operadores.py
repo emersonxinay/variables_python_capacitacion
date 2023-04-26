2459cb210201be3f6e3532393f2ba65e00b620ed
Emerson Espinoza <xinayespinoza@gmail.com>
2023-04-26 17:07:04 -0400

    mi primer commit

---
+++ operadores.py
@@ -0,0 +1,26 @@
+# operadores matematicos
+# realizar la suma 
+# datos de entrada 
+variable1 = int(input("ingrese el primer número: "))
+variable2 = int(input("ingrese el segundo número: "))
+# procesando los datos 
+suma = variable1 + variable2
+resta = variable1 - variable2
+multiplicacion = variable1*variable2
+division = variable1/variable2 
+modulo = variable1%variable2
+potencia = variable1**variable2
+# salida del proceso 
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(modulo)
+print(potencia)
+# salida con interpolación
+print(f"la suma es: {suma} ")
+print(f"la resta es: {resta} ")
+print(f"la multiplicación es: {multiplicacion} ")
+print(f"la división es: {division} ")
+print(f"el modulo es: {modulo} ")
+print(f"la potencia es: {potencia} ")
---
+++ variables.py
@@ -0,0 +1,12 @@
+# declarando variables
+nombre = "emerson"
+edad = 28
+valor_dolar = 810.20
+estudias = True
+
+# imprimiendo nuestras variables 
+print(nombre)
+print(edad)
+print(valor_dolar)
+print(estudias)
+
